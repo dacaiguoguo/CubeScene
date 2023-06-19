52b7c8b30fd411ffd5e598f09fae849750b44881
sunyanguo <dacaiguoguo@163.com>
2023-06-19 16:39:28 +0800

    Initial Commit

---
+++ CubeScene/CubeSceneApp.swift
@@ -0,0 +1,17 @@
+//
+//  CubeSceneApp.swift
+//  CubeScene
+//
+//  Created by yanguo sun on 2023/6/19.
+//
+
+import SwiftUI
+
+@main
+struct CubeSceneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
