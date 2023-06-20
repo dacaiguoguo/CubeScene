021c0425f39cdc91ee406c23be8fb79d6ee2a731
sunyanguo <dacaiguoguo@163.com>
2023-06-20 15:48:38 +0800

    更多魔方

--- CubeScene/ScenekitView.swift
+++ CubeScene/ScenekitView.swift
@@ -9,51 +9,63 @@ import SwiftUI
 import SceneKit
 
 
+extension UIColor {
+    public convenience init(hex: String) {
+        let r, g, b, a: CGFloat
+        let hex2 = "#\(hex)f0"
+        let start = hex2.index(hex2.startIndex, offsetBy: 1)
+        let hexColor = String(hex2[start...])
+
+        if hexColor.count == 8 {
+            let scanner = Scanner(string: hexColor)
+            var hexNumber: UInt64 = 0
+
+            if scanner.scanHexInt64(&hexNumber) {
+                r = CGFloat((hexNumber & 0xff000000) >> 24) / 255
+                g = CGFloat((hexNumber & 0x00ff0000) >> 16) / 255
+                b = CGFloat((hexNumber & 0x0000ff00) >> 8) / 255
+                a = CGFloat(hexNumber & 0x000000ff) / 255
+
+                self.init(red: r, green: g, blue: b, alpha: a)
+                return
+            }
+        }
+
+        self.init(red: 0, green: 0, blue: 0, alpha: 1)
+        return
+    }
+}
+
+
 struct ContentView: View {
     @State private var colorFull = true
-    let data:String = try! String(contentsOf: Bundle.main.url(forResource: "data", withExtension: "txt")!, encoding: .utf8)
-    var body: some View {
-        VStack {
-            Text(data)
-                .font(.custom("Menlo", size: 18))
-            ScenekitView(colorFull: colorFull)
-            Toggle("显示答案", isOn: $colorFull)
-        }
-        .padding()
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
-
-
-struct ScenekitView : UIViewRepresentable {
-
-    var colorFull = true;
-
-    init(colorFull: Bool = true) {
-        self.colorFull = colorFull
-    }
-
-    let scene : SCNScene = {
-        let ret = SCNScene();
-        // 添加照相机
-        let camera = SCNCamera()
-        let cameraNode = SCNNode()
-        cameraNode.camera = camera
-        cameraNode.position = SCNVector3Make(1, 1, 15)
-        ret.rootNode.addChildNode(cameraNode)
-        return ret;
-    }()
-
-    let result = {
-        let path: URL = Bundle.main.url(forResource: "data", withExtension: "txt")!
+    @State var dataIndex:Int = 0
+    let triSet:CharacterSet = {
         var triSet = CharacterSet.whitespacesAndNewlines
         triSet.insert("/")
-        let data = try! String(contentsOf: path, encoding: .utf8).trimmingCharacters(in: triSet).split(separator: "\n").map({ item in
+        return triSet
+    }()
+
+    let firstArray = {
+        let stringContent = try! String(contentsOf: Bundle.main.url(forResource: "SOMA101", withExtension: "txt")!, encoding: .utf8)
+        let firstArray = stringContent.split(separator: "/SOMA")
+        return firstArray
+    }()
+
+    func data(at: Int) -> String {
+        return String(firstArray[at])
+    }
+
+    var numberOfSoma:Int {
+        firstArray.count
+    }
+
+    func result() -> [[[Int]]] {
+        let dataStr = data(at: dataIndex)
+
+        let data = dataStr.trimmingCharacters(in: triSet).split(separator: "\n").dropFirst().filter { item in
+            item.hasPrefix("/")
+        }.map({ item in
             item.trimmingCharacters(in: triSet)
         })
 
@@ -64,20 +76,80 @@ struct ScenekitView : UIViewRepresentable {
                 }
             }
         }
-        print(result)
+        // print(result)
         return result
+    }
+
+
+    var body: some View {
+        VStack {
+            Text(data(at: dataIndex))
+                .font(.custom("Menlo", size: 18))
+            ScenekitView(colorFull: colorFull, result: result())
+            HStack {
+                Button("上一个") {
+                    dataIndex = (dataIndex - 1 + numberOfSoma) % numberOfSoma
+                    print(dataIndex)
+                }
+                Spacer()
+                Button("下一个") {
+                    dataIndex = (dataIndex + 1) % numberOfSoma
+                }
+            }
+            Toggle("显示答案", isOn: $colorFull)
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .previewDevice(PreviewDevice(rawValue: "iPhone 14"))
+            .previewDisplayName("iPhone 14")
+    }
+}
+
+
+struct ScenekitView : UIViewRepresentable {
+
+    let colorFull:Bool;
+    let result: [[[Int]]]
+
+    init(colorFull: Bool = true, result: [[[Int]]] ) {
+        self.colorFull = colorFull
+        self.result = result
+    }
+
+    let scene : SCNScene = {
+        let ret = SCNScene();
+        // 添加照相机
+        let camera = SCNCamera()
+        let cameraNode = SCNNode()
+        cameraNode.camera = camera
+        cameraNode.position = SCNVector3Make(0, 0, 15)
+        ret.rootNode.addChildNode(cameraNode)
+        return ret;
     }()
 
 
-    let colors = [
-        UIColor.black,
-        UIColor.systemCyan, // front
-        UIColor.green, // right
-        UIColor.red, // back
-        UIColor.systemIndigo, // left
-        UIColor.blue, // top
-        UIColor.purple,
-        UIColor.yellow,
+    let colors:[UIColor] = [
+//        UIColor.black,
+//        UIColor.systemCyan, // front
+//        UIColor.green, // right
+//        UIColor.red, // back
+//        UIColor.systemIndigo, // left
+//        UIColor.blue, // top
+//        UIColor.purple,
+//        UIColor.yellow,
+        UIColor(hex: "000000"),
+        UIColor(hex: "F55555"),
+        UIColor(hex: "0396FF"),
+        UIColor(hex: "EA5455"),
+        UIColor(hex: "7367F0"),
+        UIColor(hex: "32CCBC"),
+        UIColor(hex: "28C76F"),
+        UIColor(hex: "360940"),
     ] // bottom
 
     func makeUIView(context: Context) -> SCNView {
@@ -124,6 +196,10 @@ struct ScenekitView : UIViewRepresentable {
         scnView.autoenablesDefaultLighting = true
         scnView.allowsCameraControl = true
         scnView.backgroundColor = .lightGray
+//        let immm = UIImage(named: "wenli")!;
+//        let ccc = UIColor(patternImage: immm)
+//        scnView.backgroundColor = ccc
+
     }
 
 }
