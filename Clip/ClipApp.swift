3acaf5723c59c9596cf4ecfa56c2a1ff05dd435b
sunyanguo <dacaiguoguo@163.com>
2023-07-13 20:52:30 +0800

    重新测试clip

---
+++ Clip/ClipApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipApp.swift
+//  Clip
+//
+//  Created by yanguo sun on 2023/7/13.
+//
+
+import SwiftUI
+
+@main
+struct ClipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
