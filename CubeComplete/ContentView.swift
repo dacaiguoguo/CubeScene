d02ae85a2cb8892ab94ab00cf2381864daf7d818
sunyanguo <sunyanguo@lvmama.com>
2023-07-15 20:55:43 +0800

    增加自动播放功能

--- CubeComplete/ContentView.swift
+++ CubeComplete/ContentView.swift
@@ -42,23 +42,50 @@ struct ProductDetailView: View {
 
 
 struct ContentView: View {
-    @State private var age = 18 {
-        didSet {
-            print("age\(age)")
-        }
-    }
+    @State private var counter = 0
+    @State private var isTimerRunning = true
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
 
     var body: some View {
         VStack {
-//            Stepper("Enter your age", value: $age, in: 0...130)
-            Stepper("显示下一步", onIncrement: {
-                         age += 1
-                     }, onDecrement: {
-                         age -= 1
-                     })
-            Text("Your age is \(age)")
+            Text("Counter: \(counter)")
+                .font(.largeTitle)
+            
+            Button(action: {
+                isTimerRunning.toggle()
+            }) {
+                Text(isTimerRunning ? "暂停" : "继续")
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(isTimerRunning ? Color.red : Color.green)
+                    .cornerRadius(10)
+            }
+        }
+        .onReceive(timer) { _ in
+            if isTimerRunning {
+                counter += 1
+            }
         }
     }
+    
+//    @State private var age = 18 {
+//        didSet {
+//            print("age\(age)")
+//        }
+//    }
+//
+//    var body: some View {
+//        VStack {
+////            Stepper("Enter your age", value: $age, in: 0...130)
+//            Stepper("显示下一步", onIncrement: {
+//                         age += 1
+//                     }, onDecrement: {
+//                         age -= 1
+//                     })
+//            Text("Your age is \(age)")
+//        }
+//    }
 //    @State private var productList = [
 //        Product(name: "Product 1", isFavorite: true),
 //        Product(name: "Product 2", isFavorite: false),
