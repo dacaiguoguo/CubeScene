69649b4bb771f81388e60d839eb831f967e35b3f
sunyanguo <dacaiguoguo@163.com>
2023-11-01 14:21:41 +0800

    okk

--- CubeScene/ScenekitSingleView2.swift
+++ CubeScene/ScenekitSingleView2.swift
@@ -9,20 +9,15 @@ import SwiftUI
 import SceneKit
 
 
-extension SCNAction {
-    class func rotate(to location: SCNVector3, duration: TimeInterval) -> SCNAction {
-         print(location)
-        return SCNAction.rotateTo(x: CGFloat(location.x), y: CGFloat(location.y), z: CGFloat(location.z), duration: duration)
-//        let rotationVector =  // 绕Y轴旋转45度
-//        SCNAction.rotate(toX: CGFloat(location.x), y: CGFloat(location.y), z: CGFloat(location.z), duration: duration)
-//        SCNAction.rotate(toAxisAngle: SCNVector4(x: location.x, y: location.y, z: location.z, w: Float.pi / 2), duration: duration)
-
-    }
-
-}
-
 struct ScenekitSingleView2 : UIViewRepresentable {
-    let colors:[UIColor]
+    let colors:[UIColor] = [UIColor(hex: "000000"),
+                           UIColor(hex: "5B5B5B"),
+                           UIColor(hex: "C25C1D"),
+                           UIColor(hex: "2788e7"),
+                           UIColor(hex: "FA2E34"),
+                           UIColor(hex: "FB5BC2"),
+                           UIColor(hex: "FCC633"),
+                           UIColor(hex: "178E20")]
     let scene : SCNScene = {
         let ret = SCNScene();
         // 添加照相机
@@ -37,8 +32,9 @@ struct ScenekitSingleView2 : UIViewRepresentable {
         ret.background.contents = UIColor(hex: "00bfff");
         return ret;
     } ()
-    @Binding var dacai:String
-    let dataList:[Matrix3DPoint]
+
+
+    @Binding var nodeList: [SCNNode]
 
     // 创建坐标轴节点的函数
     func createAxisNode(color: UIColor, vector: SCNVector4) -> SCNNode {
@@ -53,9 +49,7 @@ struct ScenekitSingleView2 : UIViewRepresentable {
     
     func makeUIView(context: Context) -> SCNView {
         let scnView = SCNView()
-        dataList.forEach { item in
-            addNode(item)
-        }
+
         // 创建坐标轴节点
         let xAxis = createAxisNode(color: .red, vector: SCNVector4(1, 0, 0, Float.pi/2))
         let yAxis = createAxisNode(color: .green, vector: SCNVector4(0, 1, 0, Float.pi/2))
@@ -64,42 +58,15 @@ struct ScenekitSingleView2 : UIViewRepresentable {
         scene.rootNode.addChildNode(xAxis)
         scene.rootNode.addChildNode(yAxis)
         scene.rootNode.addChildNode(zAxis)
-
+        nodeList.forEach { item in
+            scene.rootNode.addChildNode(item)
+        }
         scnView.scene = scene
         scnView.autoenablesDefaultLighting = true
         scnView.allowsCameraControl = true
         return scnView
     }
     
-    func addNode(_ item: Matrix3DPoint) -> Void {
-        let countOfRow = item.data.count
-        let countOfLayer = item.data.first?.count ?? -1
-        let countOfColum = item.data.first?.first?.count ?? -1
-        let parNode2 = SCNNode()
-        for z in 0..<countOfRow {
-            for y in 0..<countOfLayer {
-                for x in 0..<countOfColum {
-                    // 盒子
-                    let box2 = SCNBox.init(width: 1, height: 1, length: 1, chamferRadius: 0.05)
-                    let value = item.data[z][y][x];
-                    if value == -1 {
-                        continue
-                    }
-                    let boxNode2 = SCNNode()
-                    boxNode2.geometry = box2
-                    boxNode2.name = "\(value)"
-                    // 由于默认y朝向上的，所以要取负值
-                    boxNode2.position = SCNVector3Make(Float(x), Float(-y), Float(z))
-                    parNode2.addChildNode(boxNode2)
-                }
-            }
-        }
-        parNode2.position = item.offset;
-        parNode2.name = item.name;
-        // 旋转此节点会导致 用手势旋转时有偏轴现象
-        // parNode2.eulerAngles = SCNVector3(-Float.pi/9, -Float.pi/6, 0)
-        scene.rootNode.addChildNode(parNode2)
-    }
 
     func updateUIView(_ scnView: SCNView, context: Context) {
         scnView.scene?.rootNode.childNodes.forEach({ node in
@@ -111,18 +78,6 @@ struct ScenekitSingleView2 : UIViewRepresentable {
                 }
             }
         })
-        let somd = self.dacai;
-        var indexofdacai = 0;
-        for (index, number) in dataList.enumerated() {
-            if number.name == somd {
-                indexofdacai = index;
-            }
-        }
-
-        let rotateAction =  SCNAction.rotate(to: dataList[indexofdacai].rotationAngle, duration: 0.1)
-        let moveAction = SCNAction.move(to: dataList[indexofdacai].offset, duration: 0.1);
-        let groupAction = SCNAction.group([moveAction, rotateAction])
-        scnView.scene?.rootNode.childNode(withName: somd , recursively: true)?.runAction(groupAction)
     }
 }
 
--- CubeScene/SingleContentView2.swift
+++ CubeScene/SingleContentView2.swift
@@ -23,10 +23,7 @@ struct Matrix3DPoint {
 
 public struct SingleContentView2: View {
 
-    @EnvironmentObject var userData: UserData
-    @State private var selectedSegment = 0
-
-    @State var dataList:[Matrix3DPoint] = [
+    static var dataList:[Matrix3DPoint] = [
         Matrix3DPoint(data: [[[1,-1,-1],],
                              [[1,1,-1], ],
                              [[-1,-1,-1],]],
@@ -72,15 +69,51 @@ public struct SingleContentView2: View {
 
     ];
 
+    @EnvironmentObject var userData: UserData
+    @State private var selectedSegment = 0
+
+
+    @State private var nodeList:[SCNNode] = {
+        dataList.map { item in
+            addNode(item)
+        }
+    }()
+
+    static func addNode(_ item: Matrix3DPoint) -> SCNNode {
+        let countOfRow = item.data.count
+        let countOfLayer = item.data.first?.count ?? -1
+        let countOfColum = item.data.first?.first?.count ?? -1
+        let parNode2 = SCNNode()
+        for z in 0..<countOfRow {
+            for y in 0..<countOfLayer {
+                for x in 0..<countOfColum {
+                    // 盒子
+                    let box2 = SCNBox.init(width: 1, height: 1, length: 1, chamferRadius: 0.05)
+                    let value = item.data[z][y][x];
+                    if value == -1 {
+                        continue
+                    }
+                    let boxNode2 = SCNNode()
+                    boxNode2.geometry = box2
+                    boxNode2.name = "\(value)"
+                    // 由于默认y朝向上的，所以要取负值
+                    boxNode2.position = SCNVector3Make(Float(x), Float(-y), Float(z))
+                    parNode2.addChildNode(boxNode2)
+                }
+            }
+        }
+        parNode2.position = item.offset;
+        parNode2.name = item.name;
+        return parNode2
+    }
+
+
     let segments = ["B 1", "B 2", "B 3", "B 4", "B 5", "B 6", "B 7"]
     @State private var dacai:String = "B 1"
     
     public var body: some View {
         VStack {
-            ScenekitSingleView2(colors: userData.colorSaveList,
-                                dacai: $dacai,
-                                dataList: dataList
-            )
+            ScenekitSingleView2(nodeList: $nodeList)
             Picker("", selection: $selectedSegment) {
                 ForEach(0 ..< segments.count, id: \.self) {
                     Text(segments[$0])
@@ -91,50 +124,68 @@ public struct SingleContentView2: View {
                     print("Selected fruit: \(segments[newValue])")
                     dacai = segments[newValue];
                 }
-            StepperViewRoute()
-            StepperView()
+            HStack {
+                Button("Rotate X") {
+                    let rotationAction = SCNAction.rotate(by: .pi / 2, around: SCNVector3(1, 0, 0), duration: 0.2)
+                    nodeList.filter({ node in
+                        node.name == dacai
+                    }).first?.runAction(rotationAction)
+                }
+                Button("Rotate Y") {
+                    let rotationAction = SCNAction.rotate(by: .pi / 2, around: SCNVector3(0, 1, 0), duration: 0.2)
+                    nodeList.filter({ node in
+                        node.name == dacai
+                    }).first?.runAction(rotationAction)
+                }
+                Button("Rotate Z") {
+                    let rotationAction = SCNAction.rotate(by: .pi / 2, around: SCNVector3(0, 0, 1), duration: 0.2)
+                    nodeList.filter({ node in
+                        node.name == dacai
+                    }).first?.runAction(rotationAction)
+                }
+            }
         }
         .padding()
     }
 
-    func StepperViewRoute() -> some View {
-        HStack {
-            Stepper("X:", value: Binding(get: {
-                dataList[selectedSegment].rotationAngle.x / (.pi / 2)
-            }, set: { newvalue, _ in
-                dataList[selectedSegment].rotationAngle.x = newvalue * (.pi / 2)
-            }), in: 0...4)
-            Stepper("Y:", value: Binding(get: {
-                dataList[selectedSegment].rotationAngle.y / (.pi / 2)
-            }, set: { newvalue, _ in
-                dataList[selectedSegment].rotationAngle.y = newvalue * (.pi / 2)
-            }), in: 0...4)
-            Stepper("Z:", value:Binding(get: {
-                dataList[selectedSegment].rotationAngle.z / (.pi / 2)
-            }, set: { newvalue, _ in
-                dataList[selectedSegment].rotationAngle.z = newvalue * (.pi / 2)
-            }), in: 0...4)
-        }
-    }
-    func StepperView() -> some View {
-        HStack {
-            Stepper("X:", value: Binding(get: {
-                dataList[selectedSegment].offset.x
-            }, set: { newvalue, _ in
-                dataList[selectedSegment].offset.x = newvalue
-            }), in: -10...10)
-            Stepper("Y:", value: Binding(get: {
-                dataList[selectedSegment].offset.y
-            }, set: { newvalue, _ in
-                dataList[selectedSegment].offset.y = newvalue
-            }), in: -10...10)
-            Stepper("Z:", value:Binding(get: {
-                dataList[selectedSegment].offset.z
-            }, set: { newvalue, _ in
-                dataList[selectedSegment].offset.z = newvalue
-            }), in: -10...10)
-        }
-    }
+//    func StepperViewRoute() -> some View {
+//        HStack {
+//            Stepper("X:", value: Binding(get: {
+//                dataList[selectedSegment].rotationAngle.x / (.pi / 2)
+//            }, set: { newvalue, _ in
+//                dataList[selectedSegment].rotationAngle.x = newvalue * (.pi / 2)
+//            }), in: 0...4)
+//            Stepper("Y:", value: Binding(get: {
+//                dataList[selectedSegment].rotationAngle.y / (.pi / 2)
+//            }, set: { newvalue, _ in
+//                dataList[selectedSegment].rotationAngle.y = newvalue * (.pi / 2)
+//            }), in: 0...4)
+//            Stepper("Z:", value:Binding(get: {
+//                dataList[selectedSegment].rotationAngle.z / (.pi / 2)
+//            }, set: { newvalue, _ in
+//                dataList[selectedSegment].rotationAngle.z = newvalue * (.pi / 2)
+//            }), in: 0...4)
+//        }
+//    }
+//    func StepperView() -> some View {
+//        HStack {
+//            Stepper("X:", value: Binding(get: {
+//                dataList[selectedSegment].offset.x
+//            }, set: { newvalue, _ in
+//                dataList[selectedSegment].offset.x = newvalue
+//            }), in: -10...10)
+//            Stepper("Y:", value: Binding(get: {
+//                dataList[selectedSegment].offset.y
+//            }, set: { newvalue, _ in
+//                dataList[selectedSegment].offset.y = newvalue
+//            }), in: -10...10)
+//            Stepper("Z:", value:Binding(get: {
+//                dataList[selectedSegment].offset.z
+//            }, set: { newvalue, _ in
+//                dataList[selectedSegment].offset.z = newvalue
+//            }), in: -10...10)
+//        }
+//    }
 }
 
 
