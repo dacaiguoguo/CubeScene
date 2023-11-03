55a886980992cb93a6b6b25e44651a4d761353ac
sunyanguo <dacaiguoguo@163.com>
2023-11-03 11:23:24 +0800

    演示成功

--- CubeScene/SingleContentView2.swift
+++ CubeScene/SingleContentView2.swift
@@ -14,7 +14,14 @@ struct Block {
     var rotation:SCNVector3
     var position:SCNVector3
     var rotationTo:SCNVector4
+    var rotationTo3:SCNVector3
     var positionTo:SCNVector3
+}
+
+enum LVAxis {
+    case x
+    case y
+    case z
 }
 
 extension SCNAction {
@@ -28,7 +35,7 @@ extension SCNAction {
         return SCNAction.rotate(by: .pi / 2, around: SCNVector3(0, 0, 1), duration: duration)
     }
 }
-
+// rotationTo 可以简化为x几次y几次z几次，用SCNVector3 记录就可以了
 //如果两个节点的旋转轴和角度都不同，那么你需要执行更复杂的计算来旋转一个节点以匹配另一个节点的方向。下面是一种通用的方法，可以帮助你实现这一目标：
 //
 //```swift
@@ -59,14 +66,14 @@ extension SCNAction {
 
 public struct SingleContentView2: View {
 
-    static let colors:[UIColor] = [UIColor(hex: "000000").withAlphaComponent(0.5),
-                                   UIColor(hex: "5B5B5B").withAlphaComponent(0.5),
-                                   UIColor(hex: "C25C1D").withAlphaComponent(0.5),
-                                   UIColor(hex: "2788e7").withAlphaComponent(0.5),
-                                   UIColor(hex: "FA2E34").withAlphaComponent(0.5),
-                                   UIColor(hex: "FB5BC2").withAlphaComponent(0.5),
-                                   UIColor(hex: "FCC633").withAlphaComponent(0.5),
-                                   UIColor(hex: "178E20").withAlphaComponent(0.5),
+    static let colors:[UIColor] = [UIColor(hex: "000000").withAlphaComponent(0.85),
+                                   UIColor(hex: "5B5B5B").withAlphaComponent(0.85),
+                                   UIColor(hex: "C25C1D").withAlphaComponent(0.85),
+                                   UIColor(hex: "2788e7").withAlphaComponent(0.85),
+                                   UIColor(hex: "FA2E34").withAlphaComponent(0.85),
+                                   UIColor(hex: "FB5BC2").withAlphaComponent(0.85),
+                                   UIColor(hex: "FCC633").withAlphaComponent(0.85),
+                                   UIColor(hex: "178E20").withAlphaComponent(0.85),
     ]
     // 算出来的第一个点就是块的位置，有了位置也就进一步确定方向，时左还是右，上还是下，是否是镜像的
     // 然后在把rotationTo改成x、y,z 旋转次数，再根据初始位置
@@ -80,6 +87,14 @@ public struct SingleContentView2: View {
     // node:Optional("块 5"),rotation:SCNVector4(x: 0.0, y: 0.99999994, z: 0.0, w: 1.5707964), position:SCNVector3(x: 0.0, y: 1.0, z: 1.0)
     // node:Optional("块 6"),rotation:SCNVector4(x: 1.0, y: 1.2665981e-07, z: 1.0803336e-07, w: 3.1415927), position:SCNVector3(x: -1.0, y: 1.0, z: 0.0)
     // node:Optional("块 7"),rotation:SCNVector4(x: 0.5773504, y: -0.57735014, z: -0.5773502, w: 2.0943954), position:SCNVector3(x: 1.0, y: 2.0, z: 0.0)
+
+    //node:块 1,rotation:SCNVector3(x: 0.0, y: 1.0, z: 1.0), position:SCNVector3(x: 1.0000011, y: 0.0, z: 0.99999976)
+    //node:块 2,rotation:SCNVector3(x: 1.0, y: 0.0, z: 3.0), position:SCNVector3(x: 0.99999964, y: 1.9999999, z: 0.99999964)
+    //node:块 3,rotation:SCNVector3(x: 0.0, y: 0.0, z: 1.0), position:SCNVector3(x: -0.99999964, y: 0.0, z: -1.0000001)
+    //node:块 4,rotation:SCNVector3(x: 3.0, y: 0.0, z: 1.0), position:SCNVector3(x: -0.9999999, y: 1.0000001, z: -1.0000001)
+    //node:块 5,rotation:SCNVector3(x: 0.0, y: 1.0, z: 0.0), position:SCNVector3(x: 3.874302e-07, y: 0.99999964, z: 0.99999964)
+    //node:块 6,rotation:SCNVector3(x: 0.0, y: 3.0, z: 2.0), position:SCNVector3(x: -3.5762787e-07, y: 0.99999964, z: -1.4901161e-07)
+    //node:块 7,rotation:SCNVector3(x: 0.0, y: 0.0, z: 2.0), position:SCNVector3(x: 1.0, y: 0.99999964, z: -1.0000001)
     static var dataList:[Block] = [
         Block(data: [[[1,-1,-1],],
                      [[1,1,-1], ],
@@ -88,7 +103,9 @@ public struct SingleContentView2: View {
               rotation: SCNVector3Zero,
               position: SCNVector3(6, 0, -5),
               rotationTo: SCNVector4(x: 0.5773504, y: -0.5773503, z: -0.5773501, w: 4.1887903),
+              rotationTo3: SCNVector3(x: 0.0, y: 1.0, z: 1.0),
               positionTo: SCNVector3(x: 1, y: 0.0, z: 1)),
+
         Block(data: [[[2,-1,-1],],
                      [[2,-1,-1],],
                      [[2,2,-1],]],
@@ -96,6 +113,7 @@ public struct SingleContentView2: View {
               rotation: SCNVector3Zero,
               position: SCNVector3(3, 0, -5),
               rotationTo: SCNVector4(x: 0.5773504, y: -0.57735014, z: -0.5773502, w: 2.0943954),
+              rotationTo3: SCNVector3(x: 1.0, y: 0.0, z: 3.0),
               positionTo: SCNVector3(x: 1, y: 2, z: 1)),
         Block(data: [[[3,-1,-1],],
                      [[3,3,-1],],
@@ -104,6 +122,7 @@ public struct SingleContentView2: View {
               rotation: SCNVector3Zero,
               position: SCNVector3(-3, 0, -5),
               rotationTo: SCNVector4(x: 0.0, y: 0.0, z: 0.99999994, w: 1.5707964),
+              rotationTo3: SCNVector3(x: 0.0, y: 0.0, z: 1.0),
               positionTo: SCNVector3(x: -1, y: 0.0, z: -1)),
         Block(data: [[[4,-1,-1],],
                      [[4,4,-1],],
@@ -112,6 +131,7 @@ public struct SingleContentView2: View {
               rotation: SCNVector3Zero,
               position: SCNVector3(0, 0, -5),
               rotationTo: SCNVector4(x: 0.5773504, y: 0.57735056, z: -0.5773499, w: 4.18879),
+              rotationTo3: SCNVector3(x: 3.0, y: 0.0, z: 1.0),
               positionTo: SCNVector3(x: 1, y: 0.0, z: -1)),
         Block(data: [[[5, -1,-1],[5,  5,-1], ],
                      [[-1,-1,-1],[-1, 5,-1],],
@@ -120,15 +140,17 @@ public struct SingleContentView2: View {
               rotation: SCNVector3Zero,
               position: SCNVector3(-3, 3, -5),
               rotationTo: SCNVector4(x: 0.0, y: 0.99999994, z: 0.0, w: 1.5707964),
+              rotationTo3: SCNVector3(x: 0.0, y: 1.0, z: 0.0),
               positionTo: SCNVector3(x: 0, y: 1, z: 1)),
-        Block(data: [[[-1, 6,-1],[ 6, 6,-1], ],
-                     [[-1,-1,-1],[ 6,-1,-1],],
+        Block(data: [[[6, -1,-1],[ 6,-1,-1], ],
+                     [[-1,-1,-1],[ 6, 6,-1],],
                      [[-1,-1,-1],[-1,-1,-1],]],
               name: "块 6",
               rotation: SCNVector3Zero,
               position: SCNVector3(3, 3, -5),
               rotationTo: SCNVector4(x: 1.0, y: 1.2665981e-07, z: 1.0803336e-07, w: 3.1415927),
-              positionTo: SCNVector3(x: -1, y: 1.0, z: 0)),
+              rotationTo3: SCNVector3(x: 0.0, y: 1.0, z: 2.0),
+              positionTo: SCNVector3(x: 0, y: 1.0, z: 0)),
         Block(data: [[[ 7,-1,-1],[ 7, 7,-1], ],
                      [[-1,-1,-1],[ 7,-1,-1],],
                      [[-1,-1,-1],[-1,-1,-1],]],
@@ -136,7 +158,8 @@ public struct SingleContentView2: View {
               rotation: SCNVector3Zero,
               position: SCNVector3(0, 3, -5),
               rotationTo: SCNVector4(x: 0.5773504, y: -0.57735014, z: -0.5773502, w: 2.0943954),
-              positionTo: SCNVector3(x: 1, y: 2, z: 0)),
+              rotationTo3: SCNVector3(x: 0.0, y: 0.0, z: 2.0),
+              positionTo: SCNVector3(x: 1, y: 1, z: -1)),
 
     ];
     let segments = {dataList.map { $0.name }}()
@@ -182,6 +205,7 @@ public struct SingleContentView2: View {
         parNode2.addChildNode(yuanNode)
         parNode2.orgPosition = item.position;
         parNode2.rotationTo = item.rotationTo;
+        parNode2.rotationTo3 = item.rotationTo3;
         parNode2.positionTo = item.positionTo;
         parNode2.name = item.name;
         return parNode2
@@ -199,7 +223,7 @@ public struct SingleContentView2: View {
 
             }
             pickerView()
-            
+
         }.navigationBarItems(trailing:completeStatus()).navigationTitle(Text("步数:\(stepcount)"))
     }
     func completeStatus() -> some View {
@@ -230,9 +254,23 @@ public struct SingleContentView2: View {
             return
         }
         let reIndex = [3,4,5,1,7,6,2][index] - 1
-        let ra = SCNAction.rotate(toAxisAngle: nodeList[reIndex].rotationTo!, duration: 1);
-        let rb = SCNAction.move(to: nodeList[reIndex].positionTo!, duration: 1);
-        nodeList[reIndex].runAction(SCNAction.group([ra, rb]), completionHandler: {
+//        let ra = SCNAction.rotate(toAxisAngle: nodeList[reIndex].rotationTo!, duration: 1);
+
+        var rolist:[SCNAction] = []
+        let rb = SCNAction.move(to: nodeList[reIndex].positionTo, duration: 1);
+        rolist.append(rb);
+        if nodeList[reIndex].rotationTo3.x > 0 {
+            rolist.append(SCNAction.rotate(by: .pi/2 * CGFloat(nodeList[reIndex].rotationTo3.x), around: SCNVector3(1, 0, 0), duration: 0.1));
+        }
+        if nodeList[reIndex].rotationTo3.y > 0 {
+            rolist.append(SCNAction.rotate(by: .pi/2 * CGFloat(nodeList[reIndex].rotationTo3.y), around: SCNVector3(0, 1, 0), duration: 0.2));
+        }
+        if nodeList[reIndex].rotationTo3.z > 0 {
+            rolist.append(SCNAction.rotate(by: .pi/2 * CGFloat(nodeList[reIndex].rotationTo3.z), around: SCNVector3(0, 0, 1), duration: 0.3));
+        }
+
+
+        nodeList[reIndex].runAction(SCNAction.sequence(rolist), completionHandler: {
             DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
                 actionmethod(index: index + 1);
             }
@@ -251,27 +289,31 @@ public struct SingleContentView2: View {
             }
         }.pickerStyle(.segmented).padding(.horizontal)
     }
+    func zhuan(_ des: LVAxis) {
+        if let aa = nodeList.filter({ node in
+            node.name == blockName
+        }).first {
+
+            switch des {
+            case .x:
+                aa.rotationTo3.x += 1;
+                aa.runAction(SCNAction.routeXPI_2(duration: 0.1))
+            case .y:
+                aa.rotationTo3.y += 1;
+                aa.runAction(SCNAction.routeYPI_2(duration: 0.1))
+            case .z:
+                aa.rotationTo3.z += 1;
+                aa.runAction(SCNAction.routeZPI_2(duration: 0.1))
+            }
+        }
+        stepcount += 1;
+    }
 
     func rotationView() -> some View {
         VStack {
-            CustomButton(title: "旋转X") {
-                nodeList.filter({ node in
-                    node.name == blockName
-                }).first?.runAction(SCNAction.routeXPI_2(duration: 0.2))
-                stepcount += 1;
-            }
-            CustomButton(title: "旋转Y") {
-                nodeList.filter({ node in
-                    node.name == blockName
-                }).first?.runAction(SCNAction.routeYPI_2(duration: 0.2))
-                stepcount += 1;
-            }
-            CustomButton(title: "旋转Z") {
-                nodeList.filter({ node in
-                    node.name == blockName
-                }).first?.runAction(SCNAction.routeZPI_2(duration: 0.2))
-                stepcount += 1;
-            }
+            CustomButton(title: "旋转X") {zhuan(.x)}
+            CustomButton(title: "旋转Y") {zhuan(.y)}
+            CustomButton(title: "旋转Z")  {zhuan(.z)}
         }.padding()
     }
 
@@ -325,7 +367,7 @@ public struct SingleContentView2: View {
 
     func lognodeInfo() -> Void {
         nodeList.forEach { node in
-            print("node:\(node.name ?? ""),rotation:\(node.rotation), position:\(node.position)")
+            print("node:\(node.name ?? ""),rotation:\(node.rotationTo3), position:\(node.position)")
         }
     }
 
@@ -333,7 +375,7 @@ public struct SingleContentView2: View {
         let generator = UIImpactFeedbackGenerator(style: .medium)
         generator.prepare()
         generator.impactOccurred()
-        // lognodeInfo()
+         lognodeInfo()
     }
 }
 
@@ -364,6 +406,7 @@ extension SCNNode {
     private struct AssociatedKeys {
         static var orgPosition = "orgPosition"
         static var rotationTo = "rotationTo"
+        static var rotationTo3 = "rotationTo3"
         static var positionTo = "positionTo"
     }
 
@@ -383,9 +426,18 @@ extension SCNNode {
             objc_setAssociatedObject(self, &AssociatedKeys.rotationTo, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
         }
     }
-    var positionTo: SCNVector3? {
+    var rotationTo3: SCNVector3 {
         get {
-            return objc_getAssociatedObject(self, &AssociatedKeys.positionTo) as? SCNVector3
+            return objc_getAssociatedObject(self, &AssociatedKeys.rotationTo3) as! SCNVector3
+        }
+        set {
+            objc_setAssociatedObject(self, &AssociatedKeys.rotationTo3, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+        }
+    }
+
+    var positionTo: SCNVector3 {
+        get {
+            return objc_getAssociatedObject(self, &AssociatedKeys.positionTo) as! SCNVector3
         }
         set {
             objc_setAssociatedObject(self, &AssociatedKeys.positionTo, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
