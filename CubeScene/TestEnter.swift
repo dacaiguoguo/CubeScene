40fdec6968e6c7f4d1572b0c65616238df970fdd
dacaiguoguo <dacaiguoguo@163.com>
2023-12-14 17:11:11 +0800

    test model

--- CubeScene/TestEnter.swift
+++ CubeScene/TestEnter.swift
@@ -7,72 +7,92 @@
 
 import SwiftUI
 
+struct TestEnterModel {
+    let name: String
+    let matrix: Matrix3D
+    
+    init(name: String, matrix: Matrix3D) {
+        self.name = name
+        self.matrix = matrix
+    }
+    init(_ name: String, _ matrix: Matrix3D) {
+        self.name = name
+        self.matrix = matrix
+    }
+}
+
+extension TestEnterModel : Identifiable {
+    var id : String {
+        name
+    }
+}
+
 // 集成到这里有点问题。。。。 2的原点不都是第一个点。。。，导致旋转方向不可靠
 struct TestEnter: View {
     
-    let items = [
-        ("right, up",
+    let items:[TestEnterModel] = [
+        TestEnterModel("right, up",
+                       transMatrix(with:
+                                    [[[-1, 2, -1], [-1, -1, -1], [-1, -1, -1]],
+                                     [[-1, 2, -1], [-1, 2, -1], [-1, 2, -1]],
+                                     [[-1, -1, -1], [-1, -1, -1], [-1, -1, -1]],])),
+        TestEnterModel("right, down",
          transMatrix(with:
-                        [[[-1, 2, -1], [-1, -1, -1], [-1, -1, -1]],
-                         [[-1, 2, -1], [-1, 2, -1], [-1, 2, -1]],
-                         [[-1, -1, -1], [-1, -1, -1], [-1, -1, -1]],])),
-                ("right, down",
-                transMatrix(with:
                         [[[-1, -1, -1], [-1, -1, -1], [-1, -1, -1]],
                          [[-1, 2, -1], [-1, 2, -1], [-1, 2, -1]],
                          [[-1, 2, -1], [-1, -1, -1], [-1, -1, -1]],])),
-        ("right, back",
-        transMatrix(with:
+        TestEnterModel("right, back",
+         transMatrix(with:
                         [[[-1, -1, -1], [-1, -1, -1], [-1, -1, -1]],
                          [[-1, 2, 2], [-1, 2, -1], [-1, 2, -1]],
                          [[-1, -1, -1], [-1, -1, -1], [-1, -1, -1]],])),
-        ("right, front",
-        transMatrix(with:
+        TestEnterModel("right, front",
+         transMatrix(with:
                         [[[-1, -1, -1], [-1, -1, -1], [-1, -1, -1]],
                          [[2, 2, -1], [-1, 2, -1], [-1, 2, -1]],
                          [[-1, -1, -1], [-1, -1, -1], [-1, -1, -1]],])),
-        ("left, up",
-        transMatrix(with:
+        TestEnterModel("left, up",
+         transMatrix(with:
                         [[[-1, -1, -1], [-1, -1, -1], [-1, 2, -1]],
                          [[-1, 2, -1], [-1, 2, -1], [-1, 2, -1]],
                          [[-1, -1, -1], [-1, -1, -1], [-1, -1, -1]],])),
-        ("left, down",
-        transMatrix(with:
+        TestEnterModel("left, down",
+         transMatrix(with:
                         [[[-1, -1, -1], [-1, -1, -1], [-1, -1, -1]],
                          [[-1, 2, -1], [-1, 2, -1], [-1, 2, -1]],
                          [[-1, -1, -1], [-1, -1, -1], [-1, 2, -1]],])),
-        ("left, back",
-        transMatrix(with:
+        TestEnterModel("left, back",
+         transMatrix(with:
                         [[[-1, -1, -1], [-1, -1, -1], [-1, -1, -1]],
                          [[-1, 2, -1], [-1, 2, -1], [-1, 2, 2]],
                          [[-1, -1, -1], [-1, -1, -1], [-1, -1, -1]],])),
-        ("left, front",
-        transMatrix(with:
+        TestEnterModel("left, front",
+         transMatrix(with:
                         [[[-1, -1, -1], [-1, -1, -1], [-1, -1, -1]],
                          [[-1, 2, -1], [-1, 2, -1], [2, 2, -1]],
                          [[-1, -1, -1], [-1, -1, -1], [-1, -1, -1]],])),
-        ("front, left",
-//        (x:1, y:1, z:2, value:2) (x:1, y:1, z:1, value:2) (x:1, y:1, z:0, value:2) (x:1, y:0, z:0, value:2)
-        transMatrix(with:
+        TestEnterModel("front, left",
+         //        (x:1, y:1, z:2, value:2) (x:1, y:1, z:1, value:2) (x:1, y:1, z:0, value:2) (x:1, y:0, z:0, value:2)
+         transMatrix(with:
                         [[[-1, -1, -1], [-1, -1, -1], [-1, -1, -1]],
                          [[-1, -1, -1], [2, 2, 2], [2, -1, -1]],
                          [[-1, -1, -1], [-1, -1, -1], [-1, -1, -1]],])),
-        ("back, up",
-//        (true, Optional((x:1, y:1, z:0, value:2)), "back, up")
-        transMatrix(with:
+        TestEnterModel("back, up",
+         //        (true, Optional((x:1, y:1, z:0, value:2)), "back, up")
+         transMatrix(with:
                         [[[-1, -1, -1], [-1, -1, 2], [-1, -1, -1]],
                          [[-1, -1, -1], [2, 2, 2],    [-1, -1, -1]],
                          [[-1, -1, -1], [-1, -1, -1], [-1, -1, -1]],])),
     ]
-
+    
     var body: some View {
-        List(items, id: \.self) { item in
+        List(items) { item in
             NavigationLink {
-                SingleContentView2(nodeList: makeNode(with: item.1))
+                SingleContentView2(nodeList: makeNode(with: item.matrix))
             } label: {
-                Text("\(items.firstIndex(of: item) ?? 0)L的方向")
+                Text("\(item.name) L的方向")
             }
-
+            
         }
     }
 }
