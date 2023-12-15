22774d82459c664c5533ffd5a10c359ccf358b84
dacaiguoguo <dacaiguoguo@163.com>
2023-12-15 14:45:26 +0800

    block 6

--- CubeScene/PointInfo.swift
+++ CubeScene/PointInfo.swift
@@ -67,6 +67,133 @@ class PointInfo : CustomDebugStringConvertible  {
         return \PointInfo.up
     }
     
+    
+    // 根据两个 KeyPath 计算第三个 KeyPath 的方法
+    class func calculateThirdKeyPath5(_ firstKeyPath: ReferenceWritableKeyPath<PointInfo, PointInfo?>,
+                                      _ secondKeyPath: ReferenceWritableKeyPath<PointInfo, PointInfo?>) -> ReferenceWritableKeyPath<PointInfo, PointInfo?>? {
+        switch (firstKeyPath, secondKeyPath) {
+        case (\PointInfo.up, \PointInfo.right):
+            return \PointInfo.back
+        case (\PointInfo.up, \PointInfo.left):
+            return \PointInfo.front
+        case (\PointInfo.up, \PointInfo.front):
+            return \PointInfo.right
+        case (\PointInfo.up, \PointInfo.back):
+            return \PointInfo.left
+            
+        case (\PointInfo.down, \PointInfo.right):
+            return \PointInfo.front
+        case (\PointInfo.down, \PointInfo.left):
+            return \PointInfo.back
+        case (\PointInfo.down, \PointInfo.front):
+            return \PointInfo.right
+        case (\PointInfo.down, \PointInfo.back):
+            return \PointInfo.left
+            
+        case (\PointInfo.left, \PointInfo.up):
+            return \PointInfo.back
+        case (\PointInfo.left, \PointInfo.down):
+            return \PointInfo.front
+        case (\PointInfo.left, \PointInfo.front):
+            return \PointInfo.up
+        case (\PointInfo.left, \PointInfo.back):
+            return \PointInfo.down
+            
+        case (\PointInfo.right, \PointInfo.up):
+            return \PointInfo.front
+        case (\PointInfo.right, \PointInfo.down):
+            return \PointInfo.back
+        case (\PointInfo.right, \PointInfo.front):
+            return \PointInfo.up
+        case (\PointInfo.right, \PointInfo.back):
+            return \PointInfo.down
+            
+        case (\PointInfo.front, \PointInfo.up):
+            return \PointInfo.right
+        case (\PointInfo.front, \PointInfo.down):
+            return \PointInfo.left
+        case (\PointInfo.front, \PointInfo.left):
+            return \PointInfo.up
+        case (\PointInfo.front, \PointInfo.right):
+            return \PointInfo.down
+            
+        case (\PointInfo.back, \PointInfo.up):
+            return \PointInfo.left
+        case (\PointInfo.back, \PointInfo.down):
+            return \PointInfo.right
+        case (\PointInfo.back, \PointInfo.left):
+            return \PointInfo.up
+        case (\PointInfo.back, \PointInfo.right):
+            return \PointInfo.down
+            
+        default:
+            return nil
+        }
+    }
+    
+    // 根据两个 KeyPath 计算第三个 KeyPath 的方法（右手定则）
+    class func calculateThirdKeyPath6(_ firstKeyPath: ReferenceWritableKeyPath<PointInfo, PointInfo?>,
+                                      _ secondKeyPath: ReferenceWritableKeyPath<PointInfo, PointInfo?>) -> ReferenceWritableKeyPath<PointInfo, PointInfo?>? {
+        switch (firstKeyPath, secondKeyPath) {
+        case (\PointInfo.up, \PointInfo.right):
+            return \PointInfo.front
+        case (\PointInfo.up, \PointInfo.left):
+            return \PointInfo.back
+        case (\PointInfo.up, \PointInfo.front):
+            return \PointInfo.left
+        case (\PointInfo.up, \PointInfo.back):
+            return \PointInfo.right
+            
+        case (\PointInfo.down, \PointInfo.right):
+            return \PointInfo.back
+        case (\PointInfo.down, \PointInfo.left):
+            return \PointInfo.front
+        case (\PointInfo.down, \PointInfo.front):
+            return \PointInfo.right
+        case (\PointInfo.down, \PointInfo.back):
+            return \PointInfo.left
+            
+        case (\PointInfo.left, \PointInfo.up):
+            return \PointInfo.back
+        case (\PointInfo.left, \PointInfo.down):
+            return \PointInfo.front
+        case (\PointInfo.left, \PointInfo.front):
+            return \PointInfo.up
+        case (\PointInfo.left, \PointInfo.back):
+            return \PointInfo.down
+            
+        case (\PointInfo.right, \PointInfo.up):
+            return \PointInfo.front
+        case (\PointInfo.right, \PointInfo.down):
+            return \PointInfo.back
+        case (\PointInfo.right, \PointInfo.front):
+            return \PointInfo.up
+        case (\PointInfo.right, \PointInfo.back):
+            return \PointInfo.down
+            
+        case (\PointInfo.front, \PointInfo.up):
+            return \PointInfo.right
+        case (\PointInfo.front, \PointInfo.down):
+            return \PointInfo.left
+        case (\PointInfo.front, \PointInfo.left):
+            return \PointInfo.up
+        case (\PointInfo.front, \PointInfo.right):
+            return \PointInfo.down
+            
+        case (\PointInfo.back, \PointInfo.up):
+            return \PointInfo.left
+        case (\PointInfo.back, \PointInfo.down):
+            return \PointInfo.right
+        case (\PointInfo.back, \PointInfo.left):
+            return \PointInfo.up
+        case (\PointInfo.back, \PointInfo.right):
+            return \PointInfo.down
+            
+        default:
+            return nil
+        }
+    }
+    
     init(x: Int, y: Int, z: Int, value: Int) {
         self.x = x
         self.y = y
@@ -84,9 +211,9 @@ func mapTo3DPointInfo(array3d: [[[Int]]]) -> [[[PointInfo]]] {
     let rows = array3d.count
     let cols = array3d[0].count
     let depth = array3d[0][0].count
-
+    
     var pointInfoArray: [[[PointInfo]]] = Array(repeating: Array(repeating: Array(repeating: PointInfo(x: 0, y: 0, z: 0, value: 0), count: depth), count: cols), count: rows)
-
+    
     for i in 0..<rows {
         for j in 0..<cols {
             for k in 0..<depth {
@@ -95,40 +222,40 @@ func mapTo3DPointInfo(array3d: [[[Int]]]) -> [[[PointInfo]]] {
             }
         }
     }
-
+    
     // 设置每个点的上、下、左、右、前、后指向
     for i in 0..<rows {
         for j in 0..<cols {
             for k in 0..<depth {
                 let currentPoint = pointInfoArray[i][j][k]
-
+                
                 if i > 0 {
                     currentPoint.up = pointInfoArray[i - 1][j][k]
                 }
-
+                
                 if i < rows - 1 {
                     currentPoint.down = pointInfoArray[i + 1][j][k]
                 }
-
+                
                 if j > 0 {
                     currentPoint.left = pointInfoArray[i][j - 1][k]
                 }
-
+                
                 if j < cols - 1 {
                     currentPoint.right = pointInfoArray[i][j + 1][k]
                 }
-
+                
                 if k > 0 {
                     currentPoint.front = pointInfoArray[i][j][k - 1]
                 }
-
+                
                 if k < depth - 1 {
                     currentPoint.back = pointInfoArray[i][j][k + 1]
                 }
             }
         }
     }
-
+    
     return pointInfoArray
 }
 
@@ -189,10 +316,36 @@ func hasContinuousEqualValues(pointInfo3DArray: [[[PointInfo]]]) -> [PointInfo] 
                         break
                     }
                 }
+                for akey in PointInfo.allKeyList {
+                    let ret = checkPoint5(currentPoint, with: 5, akeyPath: akey)
+                    if ret.0, let aa = ret.1 {
+                        aa.des = ret.2
+                        aa.pathlist = ret.3
+                        if retlist.filter({ ap in
+                            ap.value == 5
+                        }).first == nil {
+                            retlist.append(aa)
+                        }
+                        break
+                    }
+                }
+                for akey in PointInfo.allKeyList {
+                    let ret = checkPoint6(currentPoint, with: 6, akeyPath: akey)
+                    if ret.0, let aa = ret.1 {
+                        aa.des = ret.2
+                        aa.pathlist = ret.3
+                        if retlist.filter({ ap in
+                            ap.value == 6
+                        }).first == nil {
+                            retlist.append(aa)
+                        }
+                        break
+                    }
+                }
             }
         }
     }
-
+    
     return retlist
 }
 extension ReferenceWritableKeyPath where Root == PointInfo {
@@ -204,7 +357,7 @@ extension ReferenceWritableKeyPath where Root == PointInfo {
         case \PointInfo.up: return "up"
         case \PointInfo.front: return "front"
         case \PointInfo.back: return "back"
-
+            
         default: fatalError("Unexpected key path")
         }
     }
@@ -268,7 +421,7 @@ func checkPoint3(_ currentPoint:PointInfo, with checkValue: Int, akeyPath:Refere
                 children.append(contentsOf: sidepoint)
                 
                 return (true, currentPoint, "\(akeyPath.stringValue))", children)
-
+                
             }
             for akey in akey3 {
                 if let back3 = currentPoint[keyPath:akey], back3.value == checkValue {
@@ -296,7 +449,46 @@ func checkPoint4(_ currentPoint:PointInfo, with checkValue: Int, akeyPath:Refere
                 }
             }
         }
-        
+    }
+    return (false, nil, "none", [])
+}
+
+
+
+func checkPoint5(_ currentPoint:PointInfo, with checkValue: Int, akeyPath:ReferenceWritableKeyPath<PointInfo, PointInfo?>) -> (Bool, PointInfo?, String, [PointInfo]) {
+    if currentPoint.value != checkValue {
+        return (false, nil, "none", [])
+    }
+    let clist = PointInfo.checkList(akeyPath)
+    if let back1 = currentPoint[keyPath:akeyPath] , back1.value == checkValue {
+        for akey in clist {
+            if let back2 = currentPoint[keyPath:akey], back2.value == checkValue {
+                if let zuo = PointInfo.calculateThirdKeyPath5(akeyPath, akey), let back4 = back2[keyPath:zuo], back4.value == checkValue {
+                    print("currentPoint: value-\(checkValue), \(currentPoint) \(back1) \(back2)")
+                    return (true, currentPoint, "\(akeyPath.stringValue), \(akey.stringValue)", [currentPoint, back1, back2, back4])
+                }
+            }
+        }
+    }
+    return (false, nil, "none", [])
+}
+
+
+
+func checkPoint6(_ currentPoint:PointInfo, with checkValue: Int, akeyPath:ReferenceWritableKeyPath<PointInfo, PointInfo?>) -> (Bool, PointInfo?, String, [PointInfo]) {
+    if currentPoint.value != checkValue {
+        return (false, nil, "none", [])
+    }
+    let clist = PointInfo.checkList(akeyPath)
+    if let back1 = currentPoint[keyPath:akeyPath] , back1.value == checkValue {
+        for akey in clist {
+            if let back2 = currentPoint[keyPath:akey], back2.value == checkValue {
+                if let zuo = PointInfo.calculateThirdKeyPath6(akeyPath, akey), let back4 = back2[keyPath:zuo], back4.value == checkValue {
+                    print("currentPoint: value-\(checkValue), \(currentPoint) \(back1) \(back2)")
+                    return (true, currentPoint, "\(akeyPath.stringValue), \(akey.stringValue)", [currentPoint, back1, back2, back4])
+                }
+            }
+        }
     }
     return (false, nil, "none", [])
 }
